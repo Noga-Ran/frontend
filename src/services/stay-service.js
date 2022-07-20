e37cecfb127a8acd3207033a8ae5ca6034faf9cb
Noga <nogaran01@gmail.com>
2022-07-20 13:49:26 +0300

    get stays

--- src/services/stay-service.js
+++ src/services/stay-service.js
@@ -1,5 +1,6 @@
 // const fs = require('fs')
-// const stays = require('../jsons/stay.json')
+// const {stays} = require('../jsons/user.json')
+import stays from '../jsons/stay.json';
 
 import { storageService } from './async-storage-service'
 const KEY = 'stays'
@@ -14,8 +15,8 @@ export const stayService = {
 }
 
 async function query() {
-    // console.log(stays);
+    console.log(stays);
     // return await httpService.get(ENDPOINT, filterBy)
     // // return axios.get(BASE_URL, { params: { filterBy } }).then((res) => res.data)
-   return storageService.query(KEY)
+   return stays || storageService.query(KEY)
 }
