c56e8038f1ddf45d017eb72a35354190285051f0
Inbal Prital <inbalprital@gmail.com>
2022-07-21 17:38:07 +0300

    fixed category filter

--- src/cmps/category-filter.vue
+++ src/cmps/category-filter.vue
@@ -1,130 +1,127 @@
 <template>
-    <section class="category-filters-layout">
-        <section class="category-filters-container">
-            <div class="filter-img-layout">
-                <div class="filter-img-container">
-                    <img src="../assets/img/filter-imgs/islands.jpg" alt="">
-                    <div @click.prevent="choosenLabel($event)" class="filter-img-txt">
-                        Islands
-                    </div>
-                </div>
-            </div>
-            <div class="filter-img-layout">
-                <div class="filter-img-container">
-                    <img src="../assets/img/filter-imgs/beach.jpg" alt="">
-                    <div @click.prevent="choosenLabel($event)" class="filter-img-txt">
-                        Beach
-                    </div>
-                </div>
-            </div>
-            <div class="filter-img-layout">
-                <div class="filter-img-container">
-                    <img src="../assets/img/filter-imgs/amazing-pools.jpg" alt="">
-                    <div @click.prevent="choosenLabel($event)" class="filter-img-txt">
-                        Amazing pools
-                    </div>
-                </div>
-            </div>
-            <div class="filter-img-layout">
-                <div class="filter-img-container">
-                    <img src="../assets/img/filter-imgs/omg.jpg" alt="">
-                    <div @click.prevent="choosenLabel($event)" class="filter-img-txt">
-                        OMG!
-                    </div>
-                </div>
-            </div>
-            <div class="filter-img-layout">
-                <div class="filter-img-container">
-                    <img src="../assets/img/filter-imgs/national-parks.jpg" alt="">
-                    <div @click.prevent="choosenLabel($event)" class="filter-img-txt">
-                        National parks
-                    </div>
-                </div>
-            </div>
-            <div class="filter-img-layout">
-                <div class="filter-img-container">
-                    <img src="../assets/img/filter-imgs/cabins.jpg" alt="">
-                    <div @click.prevent="choosenLabel($event)" class="filter-img-txt">
-                        Cabins
-                    </div>
-                </div>
-            </div>
-            <div class="filter-img-layout">
-                <div class="filter-img-container">
-                    <img src="../assets/img/filter-imgs/campings.jpg" alt="">
-                    <div @click.prevent="choosenLabel($event)" class="filter-img-txt">
-                        Campings
-                    </div>
-                </div>
-            </div>
-            <div class="filter-img-layout">
-
-                <div class="filter-img-container">
-                    <img src="../assets/img/filter-imgs/design.jpg" alt="">
-                    <div @click.prevent="choosenLabel($event)" class="filter-img-txt">
-                        Design
-                    </div>
-                </div>
-            </div>
-            <div class="filter-img-layout">
-                <div class="filter-img-container">
-                    <img src="../assets/img/filter-imgs/tiny-homes.jpg" alt="">
-                    <div @click.prevent="choosenLabel($event)" class="filter-img-txt">
-                        Tiny homes
-                    </div>
-                </div>
-            </div>
-            <div class="filter-img-layout">
-                <div class="filter-img-container">
-                    <img src="../assets/img/filter-imgs/desert.jpg" alt="">
-                    <div @click.prevent="choosenLabel($event)" class="filter-img-txt">
-                        Desert
-                    </div>
-                </div>
-            </div>
-            <div class="filter-img-layout">
-                <div class="filter-img-container">
-                    <img src="../assets/img/filter-imgs/arctic.jpg" alt="">
-                    <div @click.prevent="choosenLabel($event)" class="filter-img-txt">
-                        Arctic
-                    </div>
-                </div>
-            </div>
-            <div class="filter-img-layout">
-                <div class="filter-img-container">
-                    <img src="../assets/img/filter-imgs/amazing-views.jpg" alt="">
-                    <div @click.prevent="choosenLabel($event)" class="filter-img-txt">
-                        Amazing views
-                    </div>
-                </div>
-            </div>
-            <div class="filter-img-layout">
-                <div class="filter-img-container">
-                    <img src="../assets/img/filter-imgs/lakefront.jpg" alt="">
-                    <div @click.prevent="choosenLabel($event)" class="filter-img-txt">
-                        Lakefront
-                    </div>
-                </div>
-            </div>
-        </section>
+  <section class="category-filters-layout">
+    <section class="category-filters-container">
+      <div class="filter-img-layout">
+        <div class="filter-img-container">
+          <img src="../assets/img/filter-imgs/islands.jpg" alt="" />
+          <div @click.prevent="choosenLabel($event)" class="filter-img-txt">
+            Islands
+          </div>
+        </div>
+      </div>
+      <div class="filter-img-layout">
+        <div class="filter-img-container">
+          <img src="../assets/img/filter-imgs/beach.jpg" alt="" />
+          <div @click.prevent="choosenLabel($event)" class="filter-img-txt">
+            Beach
+          </div>
+        </div>
+      </div>
+      <div class="filter-img-layout">
+        <div class="filter-img-container">
+          <img src="../assets/img/filter-imgs/amazing-pools.jpg" alt="" />
+          <div @click.prevent="choosenLabel($event)" class="filter-img-txt">
+            Amazing pools
+          </div>
+        </div>
+      </div>
+      <div class="filter-img-layout">
+        <div class="filter-img-container">
+          <img src="../assets/img/filter-imgs/omg.jpg" alt="" />
+          <div @click.prevent="choosenLabel($event)" class="filter-img-txt">
+            OMG!
+          </div>
+        </div>
+      </div>
+      <div class="filter-img-layout">
+        <div class="filter-img-container">
+          <img src="../assets/img/filter-imgs/national-parks.jpg" alt="" />
+          <div @click.prevent="choosenLabel($event)" class="filter-img-txt">
+            National parks
+          </div>
+        </div>
+      </div>
+      <div class="filter-img-layout">
+        <div class="filter-img-container">
+          <img src="../assets/img/filter-imgs/cabins.jpg" alt="" />
+          <div @click.prevent="choosenLabel($event)" class="filter-img-txt">
+            Cabins
+          </div>
+        </div>
+      </div>
+      <div class="filter-img-layout">
+        <div class="filter-img-container">
+          <img src="../assets/img/filter-imgs/design.jpg" alt="" />
+          <div @click.prevent="choosenLabel($event)" class="filter-img-txt">
+            Design
+          </div>
+        </div>
+      </div>
+      <div class="filter-img-layout">
+        <div class="filter-img-container">
+          <img src="../assets/img/filter-imgs/campings.jpg" alt="" />
+          <div @click.prevent="choosenLabel($event)" class="filter-img-txt">
+            Camping
+          </div>
+        </div>
+      </div>
+      <div class="filter-img-layout">
+        <div class="filter-img-container">
+          <img src="../assets/img/filter-imgs/tiny-homes.jpg" alt="" />
+          <div @click.prevent="choosenLabel($event)" class="filter-img-txt">
+            Tiny homes
+          </div>
+        </div>
+      </div>
+      <div class="filter-img-layout">
+        <div class="filter-img-container">
+          <img src="../assets/img/filter-imgs/desert.jpg" alt="" />
+          <div @click.prevent="choosenLabel($event)" class="filter-img-txt">
+            Desert
+          </div>
+        </div>
+      </div>
+      <div class="filter-img-layout">
+        <div class="filter-img-container">
+          <img src="../assets/img/filter-imgs/arctic.jpg" alt="" />
+          <div @click.prevent="choosenLabel($event)" class="filter-img-txt">
+            Arctic
+          </div>
+        </div>
+      </div>
+      <div class="filter-img-layout">
+        <div class="filter-img-container">
+          <img src="../assets/img/filter-imgs/amazing-views.jpg" alt="" />
+          <div @click.prevent="choosenLabel($event)" class="filter-img-txt">
+            Amazing views
+          </div>
+        </div>
+      </div>
+      <div class="filter-img-layout">
+        <div class="filter-img-container">
+          <img src="../assets/img/filter-imgs/lakefront.jpg" alt="" />
+          <div @click.prevent="choosenLabel($event)" class="filter-img-txt">
+            Lakefront
+          </div>
+        </div>
+      </div>
     </section>
+  </section>
 </template>
 
 <script>
-
 export default {
-    data() {
-        return {
-            label:''
-        }
+  data() {
+    return {
+      label: '',
+    }
+  },
+  components: {},
+  methods: {
+    choosenLabel(ev) {
+      this.label = ev.target.outerText
+      this.$emit('filter', this.label)
     },
-    components: {
-    },
-    methods: {
-        choosenLabel(ev){
-            this.label = ev.target.outerText
-            this.$emit('filter',this.label)
-        },
-    },
+  },
 }
 </script>
