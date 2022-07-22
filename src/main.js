2a49405fb6aa2bb6a641cc24ce35af64f75b0463
Idan Maimon <idanmaimon381891@gmail.com>
2022-07-22 18:33:21 +0300

    add a modals libary

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import router from './router'
 
 import Datepicker from '@vuepic/vue-datepicker';
 import '@vuepic/vue-datepicker/dist/main.css';
+import { vfmPlugin } from 'vue-final-modal'
 
 import './styles/style.scss'
 
@@ -13,5 +14,5 @@ const app = createApp(App)
 app.component('Datepicker', Datepicker);
 app.use(router)
 app.use(store)
+app.use(vfmPlugin)
 app.mount('#app')
- 
