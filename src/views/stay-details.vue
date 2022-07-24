cf1506a4957131f9d7a9f545c39e183a61794bae
Idan Maimon <idanmaimon381891@gmail.com>
2022-07-24 15:46:56 +0300

    pixel perfect match

--- src/views/stay-details.vue
+++ src/views/stay-details.vue
@@ -96,8 +96,8 @@
                       d="M28 4a2 2 0 0 1 1.995 1.85L30 6v7.839l1.846 5.537a3 3 0 0 1 .115.468l.03.24.009.24V30h-2v-2H2v2H0v-9.675a3 3 0 0 1 .087-.717l.067-.232L2 13.836V6a2 2 0 0 1 1.697-1.977l.154-.018L4 4zm2 18H2v4h28zm-1.388-6H3.387l-1.333 4h27.891zM28 6H4v8h2v-4a2 2 0 0 1 1.85-1.995L8 8h16a2 2 0 0 1 1.995 1.85L26 10v4h2zm-13 4H8v4h7zm9 0h-7v4h7z">
                     </path>
                   </svg>
-                  <div class="bedroom-num">bedroom-num</div>
-                  <div class="bedroom-type">bedroom-type</div>
+                  <div class="bedroom-num">bedroom 1</div>
+                  <div class="bedroom-type">king size </div>
                 </div>
               </div>
               <div class="bed-container">
@@ -108,8 +108,8 @@
                       d="M28 4a2 2 0 0 1 1.995 1.85L30 6v7.839l1.846 5.537a3 3 0 0 1 .115.468l.03.24.009.24V30h-2v-2H2v2H0v-9.675a3 3 0 0 1 .087-.717l.067-.232L2 13.836V6a2 2 0 0 1 1.697-1.977l.154-.018L4 4zm2 18H2v4h28zm-1.388-6H3.387l-1.333 4h27.891zM28 6H4v8h2v-4a2 2 0 0 1 1.85-1.995L8 8h16a2 2 0 0 1 1.995 1.85L26 10v4h2zm-13 4H8v4h7zm9 0h-7v4h7z">
                     </path>
                   </svg>
-                  <div class="bedroom-num">bedroom-num</div>
-                  <div class="bedroom-type">bedroom-type</div>
+                  <div class="bedroom-num">bedroom 2</div>
+                  <div class="bedroom-type">king size </div>
                 </div>
               </div>
               <div class="bed-container">
@@ -120,8 +120,8 @@
                       d="M28 4a2 2 0 0 1 1.995 1.85L30 6v7.839l1.846 5.537a3 3 0 0 1 .115.468l.03.24.009.24V30h-2v-2H2v2H0v-9.675a3 3 0 0 1 .087-.717l.067-.232L2 13.836V6a2 2 0 0 1 1.697-1.977l.154-.018L4 4zm2 18H2v4h28zm-1.388-6H3.387l-1.333 4h27.891zM28 6H4v8h2v-4a2 2 0 0 1 1.85-1.995L8 8h16a2 2 0 0 1 1.995 1.85L26 10v4h2zm-13 4H8v4h7zm9 0h-7v4h7z">
                     </path>
                   </svg>
-                  <div class="bedroom-num">bedroom-num</div>
-                  <div class="bedroom-type">bedroom-type</div>
+                  <div class="bedroom-num">bedroom 3</div>
+                  <div class="bedroom-type">king size </div>
                 </div>
               </div>
             </div>
