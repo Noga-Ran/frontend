e959e8b04a97d7b0eb00f4679a19c57a44ca33c0
Inbal Prital <inbalprital@gmail.com>
2022-07-25 20:36:55 +0300

    modal aminities

--- src/cmps/reviews.vue
+++ src/cmps/reviews.vue
@@ -2,11 +2,18 @@
   <section class="reviews-container details-padding">
     <h2 class="reviews-title">
       <span>
-        <svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="presentation"
-          focusable="false" style="height: 14px; width: 14px; fill: currentcolor">
+        <svg
+          viewBox="0 0 32 32"
+          xmlns="http://www.w3.org/2000/svg"
+          aria-hidden="true"
+          role="presentation"
+          focusable="false"
+          style="height: 14px; width: 14px; fill: currentcolor"
+        >
           <path
             d="M15.094 1.579l-4.124 8.885-9.86 1.27a1 1 0 0 0-.542 1.736l7.293 6.565-1.965 9.852a1 1 0 0 0 1.483 1.061L16 25.951l8.625 4.997a1 1 0 0 0 1.482-1.06l-1.965-9.853 7.293-6.565a1 1 0 0 0-.541-1.735l-9.86-1.271-4.127-8.885a1 1 0 0 0-1.814 0z"
-            fill-rule="evenodd"></path>
+            fill-rule="evenodd"
+          ></path>
         </svg>
         <span class="rating-average"> {{ getRating }} </span>
       </span>
@@ -19,7 +26,7 @@
     <ul class="reviews-list">
       <li v-for="review in stayReviewsSliced" :key="review">
         <div class="review-deatails-container">
-          <img class="profile-img" :src='review.img' alt="" />
+          <img class="profile-img" :src="review.img" alt="" />
           <div class="review-info">
             <span class="review-name">{{ review.by.fullname }}</span>
             <span class="review-time">{{ formateTime(review.at) }}</span>
@@ -29,7 +36,10 @@
           <!-- <div>{{ review.txt }}</div> -->
           <div>{{ formatedReviewTxt(review.txt) }}</div>
           <div class="show-more-review-container" v-if="this.isLongTxt">
-            <v-button class="show-more-review-btn" @click="showReviewsModal = true">
+            <v-button
+              class="show-more-review-btn"
+              @click="showReviewsModal = true"
+            >
               Show more
             </v-button>
             <span>></span>
@@ -38,39 +48,54 @@
       </li>
     </ul>
 
-    <div class="reviewsModal">
-      <vue-final-modal v-model="showReviewsModal" classes="modal-container" content-class="modal-content">
+    <div class="reviews-modal">
+      <vue-final-modal
+        v-model="showReviewsModal"
+        classes="modal-container"
+        content-class="modal-content"
+      >
         <section class="modal-container">
           <button class="modal__close" @click="showReviewsModal = false">
             X
           </button>
           <div class="modal__title">
             <div>
-            <h2 class="reviews-title">
-              <span>
-                <svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="presentation"
-                  focusable="false" style="height: 14px; width: 14px; fill: currentcolor">
-                  <path
-                    d="M15.094 1.579l-4.124 8.885-9.86 1.27a1 1 0 0 0-.542 1.736l7.293 6.565-1.965 9.852a1 1 0 0 0 1.483 1.061L16 25.951l8.625 4.997a1 1 0 0 0 1.482-1.06l-1.965-9.853 7.293-6.565a1 1 0 0 0-.541-1.735l-9.86-1.271-4.127-8.885a1 1 0 0 0-1.814 0z"
-                    fill-rule="evenodd"></path>
-                </svg>
-                <span class="rating-average"> {{ getRating }} </span>
-              </span>
-              <span>&nbsp &middot &nbsp </span>
-              <span class="review-count">{{ stay.numOfReviews }} reviews </span>
-            </h2>
+              <h2 class="reviews-title">
+                <span>
+                  <svg
+                    viewBox="0 0 32 32"
+                    xmlns="http://www.w3.org/2000/svg"
+                    aria-hidden="true"
+                    role="presentation"
+                    focusable="false"
+                    style="height: 14px; width: 14px; fill: currentcolor"
+                  >
+                    <path
+                      d="M15.094 1.579l-4.124 8.885-9.86 1.27a1 1 0 0 0-.542 1.736l7.293 6.565-1.965 9.852a1 1 0 0 0 1.483 1.061L16 25.951l8.625 4.997a1 1 0 0 0 1.482-1.06l-1.965-9.853 7.293-6.565a1 1 0 0 0-.541-1.735l-9.86-1.271-4.127-8.885a1 1 0 0 0-1.814 0z"
+                      fill-rule="evenodd"
+                    ></path>
+                  </svg>
+                  <span class="rating-average"> {{ getRating }} </span>
+                </span>
+                <span>&nbsp &middot &nbsp </span>
+                <span class="review-count"
+                  >{{ stay.numOfReviews }} reviews
+                </span>
+              </h2>
 
-            <section class="stay-reviews-scores">
-              <reviewsScores :stay="stay"></reviewsScores>
-            </section>
-</div>
+              <section class="stay-reviews-scores">
+                <reviewsScores :stay="stay"></reviewsScores>
+              </section>
+            </div>
             <ul class="reviews-list">
               <li v-for="review in reviewsCopy" :key="review">
                 <div class="review-deatails-container">
-                  <img class="profile-img" :src='review.img' alt="" />
+                  <img class="profile-img" :src="review.img" alt="" />
                   <div class="review-name-and-time">
                     <span class="review-name">{{ review.by.fullname }}</span>
-                    <span class="review-time">{{ formateTime(review.at) }}</span>
+                    <span class="review-time">{{
+                      formateTime(review.at)
+                    }}</span>
                   </div>
                 </div>
                 <div>
@@ -117,13 +142,14 @@ export default {
     }
   },
   created() {
-    
     this.reviewsCopy = JSON.parse(JSON.stringify(this.stay.reviews))
-    this.reviewsCopy.map(function(review){
+    this.reviewsCopy.map(function (review) {
       let randomNum = Math.floor(Math.random() * (99 - 1) + 1)
-      var gender = ['men','women']
-      let randomGender =Math.floor(Math.random() * (2))
-      review['img']= `https://randomuser.me/api/portraits/${gender[randomGender]}/${randomNum}.jpg`
+      var gender = ['men', 'women']
+      let randomGender = Math.floor(Math.random() * 2)
+      review[
+        'img'
+      ] = `https://randomuser.me/api/portraits/${gender[randomGender]}/${randomNum}.jpg`
       return review
     })
 
@@ -159,7 +185,7 @@ export default {
       return this.averageRating
     },
   },
-  unmounted() { },
+  unmounted() {},
 }
 </script>
 
