a9832dd118d9f4c87ca06565c7b6cc18ff5067e8
Noga <nogaran01@gmail.com>
2022-07-27 22:15:12 +0300

    add order service

--- src/services/trip-service.js
+++ src/services/trip-service.js
@@ -1,6 +1,8 @@
 
 import { storageService } from './async-storage-service'
+import { httpService } from './http.service.js'
 const KEY = 'trips'
+const ENDPOINT = 'order'
 
 export const tripService = {
     query,
@@ -13,18 +15,26 @@ export const tripService = {
 }
 
 async function query() {
-    return storageService.query(KEY)
+    // return storageService.query(KEY)
+    return await httpService.get(ENDPOINT)
 }
 
 async function remove(tripId){
-    return storageService.remove(KEY,tripId)
+    return await httpService.delete(ENDPOINT+'/'+tripId)
+    // return storageService.remove(KEY,tripId)
 }
 
 async function update(trip){
-    trip._id = Date.now()
-    storageService.post(KEY,trip)
+    var trip = JSON.parse(JSON.stringify(trip));
+  if (trip._id) {
+    return await httpService.put(`${ENDPOINT}/${trip._id}`, trip);
+  } else {
+    return await httpService.post(ENDPOINT, trip);
+  }    // trip._id = Date.now()
+    // storageService.post(KEY,trip)
 }
 
 async function getById(tripId){
-    return storageService.getById(KEY,tripId)
+    return await httpService.get(ENDPOINT+'/'+tripId)
+    // return storageService.getById(KEY,tripId)
 }
