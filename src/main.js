d27709e975829792ed73a08bcab8e779d78b282c
Inbal Prital <inbalprital@gmail.com>
2022-07-28 11:31:01 +0300

    stay map cmp

--- src/main.js
+++ src/main.js
@@ -1,16 +1,27 @@
+// const API_KEY= 'AIzaSyC4Dsp8YZjUX5Al03lrGN5TsPNvFBOvWvc'
+const API_KEY= 'AIzaSyCf4Cy6HG1TuUIpziTFMy8YWWbHrPZaAsw'
+
 import { createApp } from 'vue'
 import store from './store'
 import App from './App.vue'
 import router from './router'
-
 import Datepicker from '@vuepic/vue-datepicker'
 import '@vuepic/vue-datepicker/dist/main.css'
 import { vfmPlugin } from 'vue-final-modal'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import VueGoogleMaps from '@fawmi/vue-google-maps'
 import './styles/style.scss'
 
+
 const app = createApp(App)
+
+app.use(VueGoogleMaps, {
+    load: {
+        key: API_KEY,
+        language: 'en',
+    }
+}),
 
 app.component('Datepicker', Datepicker)
 app.use(router)
