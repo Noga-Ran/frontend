f016988b78ed0f172109459f180f2de702b16f73
Inbal Prital <inbalprital@gmail.com>
2022-07-29 09:27:49 +0300

    checking

--- src/App.vue
+++ src/App.vue
@@ -20,7 +20,7 @@ export default {
   },
   methods: {
     async login(){
-      await this.$store.dispatch({ type: "login", cred: { "username": "user", "password": "user" } })
+      await this.$store.dispatch({ type: "login", cred: { "username": "Noga", "password": "user" } })
     }
   },
 }
