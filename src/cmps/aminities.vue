bdcaa4c694fecb90b79c176385c79da9dabc7863
Noga <nogaran01@gmail.com>
2022-09-13 09:08:22 +0300

    aminities

--- src/cmps/aminities.vue
+++ src/cmps/aminities.vue
@@ -8,21 +8,12 @@
       </div>
     </section>
     <div class="amenities-modal">
-      <vue-final-modal
-        v-model="showAmenitiesModal"
-        class="modal-container"
-        content-class="modal_content"
-      >
+      <vue-final-modal v-model="showAmenitiesModal" class="modal-container" content-class="modal_content">
         <section class="modal-container">
           <div class="close-modal">
             <button class="modal__close" @click="showAmenitiesModal = false">
-              <svg
-                viewBox="0 0 32 32"
-                xmlns="http://www.w3.org/2000/svg"
-                aria-hidden="true"
-                role="presentation"
-                focusable="false"
-                style="
+              <svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="presentation"
+                focusable="false" style="
                   display: block;
                   fill: none;
                   height: 16px;
@@ -30,8 +21,7 @@
                   stroke: currentcolor;
                   stroke-width: 3;
                   overflow: visible;
-                "
-              >
+                ">
                 <path d="m6 6 20 20"></path>
                 <path d="m26 6-20 20"></path>
               </svg>
@@ -43,11 +33,7 @@
               <section class="amenities-list">
                 <div v-for="amenitie in stayAmenities" :key="amenitie">
                   <div class="modal-amenitie">
-                    <img
-                      class="amenitie-svg"
-                      :src="getImgUrl(amenitie)"
-                      alt=""
-                    />
+                    <img class="amenitie-svg" :src="getImgUrl(amenitie)" alt="" />
                     {{ amenitie }}
                   </div>
                   <hr class="seperating-line" />
@@ -92,9 +78,23 @@ export default {
   methods: {
     getImgUrl(amenitie) {
       var formatedAmenitie = amenitie.replaceAll(' ', '-')
-      var imgUrl = `https://res.cloudinary.com/nogacloud/image/upload/v1659275294/aminities/${formatedAmenitie}.svg`
+      formatedAmenitie = formatedAmenitie.replaceAll('/', '-')
+      formatedAmenitie = formatedAmenitie.replaceAll('(', '')
+      formatedAmenitie = formatedAmenitie.replaceAll(')', '')
+      formatedAmenitie = formatedAmenitie.replaceAll('’', '')
+
+      let imgUrl = `https://res.cloudinary.com/nogacloud/image/upload/aminities/${formatedAmenitie}.svg`
+      let isExists = this.imageExists(imgUrl);
+      if(isExists) return new URL(imgUrl, import.meta.url).href
+      imgUrl = `https://res.cloudinary.com/nogacloud/image/upload/aminities/defualt.svg`
       return new URL(imgUrl, import.meta.url).href
     },
+    imageExists(image_url){
+    var http = new XMLHttpRequest();
+    http.open('HEAD', image_url, false);
+    http.send();
+    return http.status != 404;
+  }
   },
   computed: {},
   unmounted() {},
